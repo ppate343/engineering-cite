6ffd28fc3eb0f3f1d54f5938893fdd2c9c81fede
ppate343 <71097041+ppate343@users.noreply.github.com>
2022-02-08 00:00:27 -0500

    image square container

--- src/components/Devices.jsx
+++ src/components/Devices.jsx
@@ -1,8 +1,56 @@
 import React from "react";
 import DeviceDiagram from "../assets/img/devices/device-diagram.jpg";
 import SystemDiagram from "../assets/img/devices/system-highlights.jpg";
+import { makeStyles } from "@material-ui/core";
 
-export default ({ deviceLinks }) => {
+
+
+
+
+
+const useStyles = makeStyles(() => ({
+  container: {
+    // justifyContent: 'center',
+
+    backgroundColor: 'transparent',
+    height: 800,
+    width: 'auto',
+  },
+
+  leftBox: {
+
+    backgroundColor: "black",
+    height: 700,
+    width: 600,
+    marginRight: 400,
+
+
+  },
+  RightBox: {
+
+    position: 'absolute',
+    backgroundColor: 'white',
+    height: 500,
+    width: 600,
+    marginTop: 100,
+    marginLeft: 500,
+
+
+  }
+}))
+
+
+
+
+
+
+
+
+
+export default () => {
+
+  const style = useStyles();
+
   return (
     <section className="page-section" id="devices">
       <div className="container">
@@ -14,15 +62,11 @@ export default ({ deviceLinks }) => {
             measured data to local and remote destinations as desired.
           </p>
         </div>
-        <div className='row squarepicturelight' >
-          <div className='small-square-light'>
-            <div className="small-square-pic-light">
-              <img src={DeviceDiagram}></img>
-
-            </div>
-            <p className="text-muted">            Multiple diverse wireless channels for reliable data transfer</p>
-
+        <div className={style.container}>
+          <div className={style.RightBox}>
           </div>
+          <div className={style.leftBox}>
+          </div >
 
         </div>
       </div>
